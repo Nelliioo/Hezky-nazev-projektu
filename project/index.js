be9cbcb767010765c888778d1ac031fe305f83f4
Nelliioo <cidlinovanela@gmail.com>
2023-07-20 10:54:01 +0200

    Co-authored-by: Czicken <Czicken@users.noreply.github.com> Co-authored-by: AdelKN <AdelKN@users.noreply.github.com> Co-authored-by: LuciusZeta <LuciusZeta@users.noreply.github.com>

--- project/index.js
+++ project/index.js
@@ -1,17 +1,3 @@
-
-let topbutton = document.getElementById("topbutton");
-
-
-window.onscroll = function() {scroll()};
-
-function scroll() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    topbutton.style.display = "block";
-  } else {
-    topbutton.style.display = "none";
-  }
-}
-
 function top() {
-  document.documentElement.scrollTop = 0;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
