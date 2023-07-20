c34aa079309392b5a4c1a4ba6fdbe5001f3cd3e7
Nelliioo <cidlinovanela@gmail.com>
2023-07-20 09:43:42 +0200

    dgfd

--- project/index.js
+++ project/index.js
@@ -1,5 +1,17 @@
-let topbutton = document.getElementById("topbutton")
 
-window.onscroll() = function {scrollFunction()};
+let topbutton = document.getElementById("topbutton");
 
-fun
+
+window.onscroll = function() {scroll()};
+
+function scroll() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+function top() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
