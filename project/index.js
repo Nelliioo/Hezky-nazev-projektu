d95c77704b667f275b34fe51b994de4be567ee44
Nelliioo <cidlinovanela@gmail.com>
2023-07-20 11:06:54 +0200

    sddsd

--- project/index.js
+++ project/index.js
@@ -1,7 +1,3 @@
 function top(){
-    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if (currentScroll > 0) {
-         window.requestAnimationFrame(smoothscroll);
-         window.scrollTo (0,currentScroll - (currentScroll/5));
-    }
-})();
+    document.documentElement.scrollTop = 0;
+};
